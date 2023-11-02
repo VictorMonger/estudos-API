98a058a70c80ebdc60ed1d9a9e98df73bc0feef2
Victor Monger <victormonger2@outlook.com>
2023-11-02 15:24:30 -0300

    error changes

--- src/index.js
+++ src/index.js
@@ -31,10 +31,20 @@ app.get('/multiplication', (request, response) => {
 
 app.get('/ratio', (request, response) => {
   const { number1, number2 } = request.query;
+  
+  const number1int = parseInt(number1);
+  const number2int = parseInt(number2);
 
-  const ratioResult = parseInt(number1) / parseInt(number2);
+  try {
+    if (number2int === 0){
+      throw new Error ('Divisor não pode ser igual a zero.')
+    }
+    const ratioResult = number1int / number2int;
+    response.send({ ratioResult });
 
-  response.send({ ratioResult });
+  } catch (error) {
+    response.status(400).send({ error: error.message });
+  }
 });
 
 app.listen(3000);
