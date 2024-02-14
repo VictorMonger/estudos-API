ca142f405e9221ef9d6cbde4d719c6b1130ce70a
Victor Monger <victormonger2@outlook.com>
2024-02-14 17:39:56 -0300

    describe added

--- test/math.test.js
+++ test/math.test.js
@@ -1,17 +1,25 @@
-const { sum, subtraction, multiplication, division } = require('../src/math');
+const { sum, subtraction, multiplication, division } = require("../src/math");
 
-test('sum should be correct', () => {
-  expect(sum(1, 2)).toBe(3);
+describe("sum module", () => {
+  test("sum should be correct", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
 });
 
-test('subtraction should be correct', () => {
-  expect(subtraction(3, 2)).toBe(1);
+describe("subtraction module", () => {
+  test("subtraction should be correct", () => {
+    expect(subtraction(3, 2)).toBe(1);
+  });
 });
 
-test('multiplication should be correct', () => {
-  expect(multiplication(4, 2)).toBe(8);
+describe("multiplication module", () => {
+  test("multiplication should be correct", () => {
+    expect(multiplication(4, 2)).toBe(8);
+  });
 });
 
-test('division should be correct', () => {
-  expect(division(8, 4)).toBe(2);
+describe("division module", () => {
+  test("division should be correct", () => {
+    expect(division(8, 4)).toBe(2);
+  });
 });
